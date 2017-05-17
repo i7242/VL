0c9df28af0bfb9f4ef012e33028db299e2e396f2
17242 <xingyuyan@outlook.com>
2017-05-17 14:10:22 +0200

    Added new function, but not tested yet

--- Test_Caliper/ASSOCIATION.lua
+++ Test_Caliper/ASSOCIATION.lua
@@ -434,6 +434,220 @@ elseif (measurement_type=='Caliper_B') then
 	output('Distance',Distance)
 
 elseif (measurement_type=='Caliper_C') then
+	-- measurement using the top two surfaces------------------------
+	
+	-- criteria to stop association----------------------------------
+	--[[	here CRT is the absolute value of
+			relative displacement between the caliper and slide. ]]--
+	local CRT=1
+	local loop_num=1
+	local empty=true
+
+	--[[	measurement points on each plane
+			position is relative to the
+			plane center								         ]]--
+	
+	-- r correspond to "z" direction
+	local num_r=8
+	--	!!! notice the direction for row
+	--	negetive direction is the longer direction
+	local range_r1=-0.003
+	local range_r2= 0.003
+	local dr=(range_r2-range_r1)/(num_r-1)
+	-- c correspond to "y" direction
+	local num_c=4
+	local range_c1=-0.001
+	local range_c2= 0.001
+	local dc=(range_c2-range_c1)/(num_c-1)
+
+	-- loop to associate---------------------------------------------
+	while ((CRT>1e-5) and (loop_num<10)) do
+		-- construct measurement points on caliper-------------------
+		--[[	this point coordinate is relative to the
+				center of measurement surface
+				no relation to the global coordinate.			 ]]--
+		local PTS=matrix(num_r*num_c,3,0)
+		local count=1
+		for i=1,num_r do
+			pst_r=range_r1+dr*(i-1)
+			for j=1,num_c do
+				pst_c=range_c1+dc*(j-1)
+				PTS[count][1]=0
+				PTS[count][2]=pst_c
+				PTS[count][3]=pst_r
+				count=count+1
+			end
+		end
+
+		--[[	V_normal_1:	normal direction of the
+				first measurement plane it is generated
+				by rotation from initial position     			 ]]--
+		local a=Caliper:getOrientation()
+		local V_normal_1=Vec3(1,0,0)
+			  V_normal_1=a:rotate(V_normal_1)
+		local V_normal_2=V_normal_1
+		--[[	dir_c2: direction from part center to the
+				center of measurement plane						 ]]--
+		local dir_c2=Vec3(-0.015,0.001,-0.055)
+			  dir_c2=a:rotate(dir_c2)
+		--[[	dir_c3: direction from one measurement surface
+				center to the other surface center				 ]]--
+		local dir_c3=Vec3(0,-0.002,0)
+			  dir_c3=a:rotate(dir_c3)
+	
+		-- get position of two part center---------------------------
+		local PS1=Caliper:getStagePosition()
+		local PS2=Slide:getStagePosition()
+		-- distance between two planes-------------------------------
+		--	"0.025" is the initial distance, when
+		--	the caliper is "closed"
+		local DPL=(PS2-PS1)*V_normal_2
+		DPL=DPL-0.025
+		-- change to actual measurement surface position-------------
+		PS1=PS1+dir_c2
+		PS2=PS1-V_normal_2*DPL+dir_c3
+
+		-- measurement distance D1 by ray----------------------------
+		--	give D1 initially a large value
+		--	if find no intersection point, using this value
+		--	and the constraint will be satisfied
+		local D1=matrix(num_r*num_c,1,9e9)
+		for i=1,(num_r*num_c) do
+			local MPT=Vec3(PTS[i][1],PTS[i][2],PTS[i][3])
+			MPT=a:rotate(MPT)
+			--	slite move along normal direction, to avoid
+			--	"rayIntersect" find no point
+			MPT=MPT+PS1-V_normal_1*0.001
+			local r = Ray(MPT, V_normal_1)
+			local t = rayIntersect(Stage.SceneNode, r, false, true)
+
+			if (t~= nil) then
+				local num_t=1
+				local num_d=0
+				local d1={}
+				while (t[num_t]~=nil) do
+					if ((t[num_t].Entity.Name~='Caliper')and(t[num_t].Entity.Name~='Slide')) then
+						num_d=num_d+1
+						--	"-0.001" corresponding to before
+						--	a distance was added to avoid empty intersection
+						d1[num_d]=t[num_t].IntersectedPoint.distance-0.001
+					end
+					num_t=num_t+1
+				end
+			
+				if (num_d>0) then
+					D1[i][1]=d1[1]
+				end
+				
+				if (num_d>1) then
+					for j=2,num_d do
+						if (math.abs(d1[j])<math.abs(D1[i][1])) then
+							D1[i][1]=d1[j]
+						end
+					end
+				end
+			
+			end
+		
+			if (D1[i][1]<DPL) then
+				empty=false
+			end
+
+		end
+	
+		-- measurement distance D2 by ray----------------------------
+		local D2=matrix(num_r*num_c,1,9e9)
+		for i=1,(num_r*num_c) do
+			local MPT=Vec3(PTS[i][1],PTS[i][2],PTS[i][3])
+			MPT=a:rotate(MPT)
+			MPT=MPT+PS2-V_normal_2*0.001
+			local r = Ray(MPT, V_normal_2)
+			local t = rayIntersect(Stage.SceneNode, r, false, true)
+			
+			if (t~= nil) then
+				local num_t=1
+				local num_d=0
+				local d2={}
+				while (t[num_t]~=nil) do
+					if ((t[num_t].Entity.Name~='Caliper')and(t[num_t].Entity.Name~='Slide')) then
+						num_d=num_d+1
+						d2[num_d]=t[num_t].IntersectedPoint.distance-0.001
+					end
+					num_t=num_t+1
+				end
+				
+				if (num_d>0) then
+					D2[i][1]=d2[1]
+				end
+				
+				if (num_d>1) then
+					for j=2,num_d do
+						if (math.abs(d2[j])<math.abs(D2[i][1])) then
+							D2[i][1]=d2[j]
+						end
+					end
+				end
+				
+			end
+		
+			if (D2[i][1]<DPL) then
+				empty=false
+			end
+
+		end
+
+		-- conduct association---------------------------------------
+		if (empty) then
+			print('Parts beyond the measurement range!')
+			Result=matrix(7,1,0)
+			Result[7][1]=-DPL
+			CRT=0
+		else
+			Result,CRT = metrology.Caliper_B(D1,D2,DPL,PTS)
+		end
+
+		-- translate the position of caliper-------------------------
+		--[[	translation corresponding to the left
+				contact surface of the caliper.					 ]]--
+		local tr_c2=Vec3(Result[1][1],Result[2][1],Result[3][1])
+		local ro_c2=Vec3(Result[4][1],Result[5][1],Result[6][1])
+		local tr_c1=tr_c2+dir_c2:cross(ro_c2)
+			  tr_c1.x=-tr_c1.x
+			  tr_c1.z=-tr_c1.z
+		--[[	7th variable of SDT is the changing
+				of distance between two faces.
+				positive is increasing distance.				 ]]--
+		local tr_s=Vec3(Result[7][1],0,0)
+		Caliper:translateLocal(tr_c1)
+		Slide:translateParent(tr_s)
+		
+		-- rotation the cqliper, Vec3(y,x,z)-------------------------
+		--[[	assume the rotation angle is small,
+				the SDT is used as
+				Eular Angle directly.							 ]]--
+		local RTQT=Quat(0,0,0,1)
+		RTQT:setEulerAngles(Vec3(-Result[5][1],Result[4][1],-Result[6][1]))
+		Caliper:rotateLocal(RTQT)
+		
+		loop_num=loop_num+1
+		
+	end
+
+	--	Re-calculate the distance between planes of caliper and slide
+	--	it is the same as in "Caliper_A"
+	local V_normal_1=Vec3(1,0,0)
+	local a=Caliper:getOrientation()
+	V_normal_1=a:rotate(V_normal_1)
+	
+	local	PS1=Caliper:getStagePosition()
+	local PS2=Slide:getStagePosition()
+	
+	local DPL=(PS2-PS1)*V_normal_1
+	Distance=(DPL-0.025)*1000
+	
+	--	output the result
+	output('Distance',Distance)
+elseif (measurement_type=='Caliper_D') then
 
 elseif (measurement_type=='Micrometer') then
 	-- "MicroMeter_A" & "MicroMeter_B" are used as the two parts-----
--- Test_Caliper/metrology.lua
+++ Test_Caliper/metrology.lua
@@ -299,6 +299,207 @@ function metrology.Caliper_B(D1,D2,DPL,PTS)
 end
 -- -------------------------------------------------
 
+-- Function Associate-------------------------------
+function metrology.Caliper_C(D1,D2,DPL,PTS,dir_c3)
+    
+    -- Caliper_C:-----------------------------------
+    --  conduct measurement by two measurement planes
+    --  these planes are on the top of caliper
+    --  the maximum distance between these planes and
+    --  measurement poits are minimized
+    -- ---------------------------------------------
+
+    -- Inputs:--------------------------------------
+    --  D1: distance between 1st measurement plane & skin model shape
+    --  D2: distance between 2nd measurement plane & skin model shape
+    --  DPL: distance between 1st & 2nd plane
+    --  PTS: measurement points on 1st plane
+    -- ---------------------------------------------
+    
+    -- Define the number of "contact points"--------
+    local a,b = matrix.size(PTS)
+    local n_ctpts=a
+    -- ---------------------------------------------
+
+    -- Definition of the 1st plane------------------
+    --  C1: center point
+    --  N1: normal vector
+    --  P1: position of measurement contact points
+    --      this is relative to "C1"
+    --      and should be same as in "D1"
+    local C1=matrix{0,0,0}
+    local N1=matrix{1,0,0}
+    local P1=PTS
+    -- ---------------------------------------------
+
+    -- Definition of the 2nd plane------------------
+    --  similar to 1st plane
+    -- ??? using dir_c3 ???
+    local C2=matrix{-DPL,0,0}
+    local N2=matrix{1,0,0}
+    local P2=PTS
+    for i=1,n_ctpts do
+        P2[i][1]=-DPL
+    end
+    -- ---------------------------------------------
+
+    -- Formulating the linear programming-----------
+    --  {Tx1,Tx2,Ty1,Ty2,Tz1,Tz2,Rx1,Rx2,Ry1,Ry2,Rz1,Rz2,DA1,DA2,Dmax}
+    --  15 variables
+    --  "1" will be plus
+    --  "2" will be minus
+    --  Tx,Ty,Tz: translation torsor
+    --  Rx,Ry,Rz: rotation torsor
+    --  DA1: displacement along normal direction of the second plane
+    --  DA2: inverse displacement of DA1
+    --  "Dmax" will be minimized
+    
+    -- For each plane
+    --  there are matrial conditions
+    --  there are "Dmax" conditions
+    --  and there are two planes
+    --  and one minimization objective
+    --  all equations are in form "<="
+    --  no "=" or ">="
+    
+    --  So the row number of matrix "A" is:
+    --      4*n_ctpts+2  (one column more to avoide error in require nil value)
+    --  the column number of matrix "A" is:
+    --      15+2*n_ctpts+2
+    local A=matrix(4*n_ctpts+2,15+2*n_ctpts+2,0)
+    
+    -- This is for the 1st measurement plane--------
+    for i=1,n_ctpts do
+        -- material conditions----------------------
+        A[2*i-1][1+1]=N1[1][1]
+        A[2*i-1][2+1]=-N1[1][1]
+
+        A[2*i-1][3+1]=N1[2][1]
+        A[2*i-1][4+1]=-N1[2][1]
+
+        A[2*i-1][5+1]=N1[3][1]
+        A[2*i-1][6+1]=-N1[3][1]
+
+        local r=matrix{P1[i][1],P1[i][2],P1[i][3]}+N1*D1[i][1]
+        r=r:cross(N1)
+        A[2*i-1][7+1]=r[1][1]
+        A[2*i-1][8+1]=-r[1][1]
+
+        A[2*i-1][9+1]=r[2][1]
+        A[2*i-1][10+1]=-r[2][1]
+
+        A[2*i-1][11+1]=r[3][1]
+        A[2*i-1][12+1]=-r[3][1]
+
+        A[2*i-1][15+2*n_ctpts+2]=D1[i][1]
+
+        -- "Dmax" conditions----------------------
+        A[2*i][1+1]=-N1[1][1]
+        A[2*i][2+1]=N1[1][1]
+
+        A[2*i][3+1]=-N1[2][1]
+        A[2*i][4+1]=N1[2][1]
+
+        A[2*i][5+1]=-N1[3][1]
+        A[2*i][6+1]=N1[3][1]
+
+        local r=matrix{P1[i][1],P1[i][2],P1[i][3]}+N1*D1[i][1]
+        r=r:cross(N1)
+        A[2*i][7+1]=-r[1][1]
+        A[2*i][8+1]=r[1][1]
+
+        A[2*i][9+1]=-r[2][1]
+        A[2*i][10+1]=r[2][1]
+
+        A[2*i][11+1]=-r[3][1]
+        A[2*i][12+1]=r[3][1]
+
+        A[2*i][15+1]=-1
+
+        A[2*i][15+2*n_ctpts+2]=0
+    end
+    
+    -- This is for the 2nd plane--------------------
+    -- similar to the first one
+    -- but the changing of distance between 2 planes
+    -- are considered
+    for i=n_ctpts+1,2*n_ctpts do
+        -- material conditions----------------------
+        A[2*i-1][1+1]=N2[1][1]
+        A[2*i-1][2+1]=-N2[1][1]
+
+        A[2*i-1][3+1]=N2[2][1]
+        A[2*i-1][4+1]=-N2[2][1]
+        
+        A[2*i-1][5+1]=N2[3][1]
+        A[2*i-1][6+1]=-N2[3][1]
+        
+        local r=matrix{P2[i-2*n_ctpts][1],P2[i-2*n_ctpts][2],P2[i-2*n_ctpts][3]}+N2*D2[i-n_ctpts][1]-C2
+        r=r:cross(N2)
+        A[2*i-1][7+1]=r[1][1]
+        A[2*i-1][8+1]=-r[1][1]
+
+        A[2*i-1][9+1]=r[2][1]
+        A[2*i-1][10+1]=-r[2][1]
+
+        A[2*i-1][11+1]=r[3][1]
+        A[2*i-1][12+1]=-r[3][1]
+
+        A[2*i-1][13+1]=1
+        A[2*i-1][14+1]=-1
+        
+        A[2*i-1][15+2*n_ctpts+2]=D2[i-n_ctpts][1]
+
+        -- "Dmax" conditions----------------------
+        A[2*i][1+1]=-N1[1][1]
+        A[2*i][2+1]=N1[1][1]
+
+        A[2*i][3+1]=-N1[2][1]
+        A[2*i][4+1]=N1[2][1]
+
+        A[2*i][5+1]=-N1[3][1]
+        A[2*i][6+1]=N1[3][1]
+
+        local r=matrix{P2[i-2*n_ctpts][1],P2[i-2*n_ctpts][2],P2[i-2*n_ctpts][3]}+N2*D2[i-n_ctpts][1]-C2
+        r=r:cross(N2)
+        A[2*i][7+1]=-r[1][1]
+        A[2*i][8+1]=r[1][1]
+
+        A[2*i][9+1]=-r[2][1]
+        A[2*i][10+1]=r[2][1]
+
+        A[2*i][11+1]=-r[3][1]
+        A[2*i][12+1]=r[3][1]
+
+        A[2*i][15+1]=-1
+
+        A[2*i][15+2*n_ctpts+2]=0
+    end
+
+    --  Add objective line----------------------
+    --  minimize the maximum distance "Dmax"
+    A[4*n_ctpts+1][15+1]=1
+
+    -- Using Simplex----------------------------
+    local L=4*n_ctpts
+    local E=0
+    local G=0
+    local N=15
+    local F=-1
+
+    Result, Objective=optimize.simplex(L,E,G,N,F,A)
+
+    local Result2=matrix(7,1,0)
+    for i=1,7 do
+        Result2[i][1]=Result[2*i-1][1]-Result[2*i][1]
+    end
+
+    return Result2,math.abs(Result2[7][1])
+
+    -- ---------------------------------------------
+end
+-- -------------------------------------------------
+
 return metrology
 
 -- -------------------------------------------------
